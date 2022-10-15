fe9834e86d605b14e628695426f23c058bec512a
Yuvixa <ycaminazana@gmail.com>
2022-10-14 22:58:11 -0500

    Video-03-Create-React-App

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> amazona</a>
+        <main>list products</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
